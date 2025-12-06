9078db454229fb2d2b31164802321fc7b5e28f1e
arpit-golakiya <arpitgolakiya97@gmail.com>
2025-12-06 17:04:56 +0530

    fix: minor auto deploy changes.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-        unoptimized: true,
-    },
+    distDir: 'export',
+    images: { unoptimized: true },
   /* config options here */
 };
 
